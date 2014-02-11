5751a1cdac69d592f0da3d1017db7cd86b278638
boryana <batanaso@engr-131-04.eastern.ewu.edu>
2014-02-11 14:56:55 -0800

    updates

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).foundation();
+
+// Transit Plugin
+// $('.box').transition({ x: -40 });
+
+// Animate.css
+// $('.box').addClass('animated fadeInDownBig');
+
+// console.log("OK USA!");
